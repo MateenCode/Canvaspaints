630a312c9cc58d43cfcb668fba367bf910d95cec
Mateen Kazia <mateencode@gmail.com>
2019-03-19 20:08:03 -0700

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./css/App.scss";
 
+// components
+import Header from "./components/Header";
+
 class App extends Component {
   render() {
-    return <div className="App">hello</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Collapse, Navbar } from "reactstrap";
+import { NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
+import Logo from "../assets/icon/logo.svg";
+import Help from "../assets/icon/help.svg";
+import Cart from "../assets/icon/cart.svg";
+import Bars from "../assets/icon/bars.svg";
+import "../css/Header.scss";
+
+export default class Header extends Component {
+  state = {
+    collapsed: true
+  };
+
+  toggleNavbar = () => {
+    this.setState(state => ({
+      collapsed: !state.collapsed
+    }));
+  };
+
+  render() {
+    return (
+      <div className="header">
+        <Navbar light expand="md">
+          <span onClick={this.toggleNavbar}>
+            <img src={Bars} alt="n/a" className="logo_svg" />
+          </span>
+          <img src={Logo} alt="n/a" className="logo_svg" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="">About</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          <NavbarBrand>
+            <img src={Help} alt="n/a" className="icon_svg mr-2" />
+            <img src={Cart} alt="n/a" className="icon_svg" />
+          </NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
+}
